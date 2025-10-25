7c9f4dc88d0b8495cb28be97f948a7b4dbca2f17
Thayrony Von Held <thayronyvonheld@gmail.com>
2025-10-24 23:49:37 -0300

    Ideia

---
+++ src-projects/oop/Lesson2/JokenPo/Main.java
@@ -0,0 +1,15 @@
+package oop.Lesson2.JokenPo;
+
+public class Main {
+}
+
+/*
+Refazer o exercicio de logica em C, porém agora utilizando Classes e objetos, metodos... Pensei em algo como Classe Jogo,
+Objetos, pedra, papel, tesoura. E seus metodos,
+win sla
+loser
+pedra perde para papel,  ganha de tesoura,
+Papel ganha de pedra, e perde para tesoura
+tesoura ganha de papel e perde para pedra
+
+ */
