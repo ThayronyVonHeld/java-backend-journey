1cf1d0fb2b2ca7e3eae027726e33fb21413b1a43
Thayrony Von Held <thayronyvonheld@gmail.com>
2025-11-26 23:43:49 -0300

    Update

--- src-projects/AulaExtra_SistemaBanco/ContaBanco.java
+++ src-projects/AulaExtra_SistemaBanco/ContaBanco.java
@@ -2,6 +2,8 @@ package AulaExtra_SistemaBanco;
 
 import javafx.scene.Parent;
 
+import java.util.Scanner;
+
 public class ContaBanco {
 
     public int NumConta;
@@ -14,16 +16,37 @@ public class ContaBanco {
     Status = true;
     }
 
-    public void fecharConta{
+    public void fecharConta(){
         if(saldo == 0){
         Status = false;
+            System.out.println("Sua Conta foi Fechada");
         }
-        if else(saldo < 0){
+       else if (saldo < 0){
             System.out.println("Sua conta Posui saldo negativo, para fechar, deposite o valor restante");
         }
         else{
             System.out.println("Saque todo o valor da sua conta para fechar");
         }
     }
+    public double depositar(){
+        double deposito = 0;
+        System.out.println("Digite o valor que deseja depositar: ");
+        Scanner teclado = new Scanner(System.in);
+        deposito = teclado.nextDouble();
+        return deposito;
+    }
+public double sacar(){
+    double saque = 0;
+        if(saldo > 0) {
+            System.out.println("Digite o valor que deseja sacar: ");
+            Scanner teclado = new Scanner(System.in);
+            saque = teclado.nextDouble();
+        }
+        else {
+            System.out.println("Sem saldo Disponivel para sacar ");
+        }
+    return saque;
+    }
+
 
 }
