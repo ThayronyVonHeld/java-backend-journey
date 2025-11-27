a32563ca25b1177f2f56d4662dac5d646a4ce93b
Thayrony Von Held <thayronyvonheld@gmail.com>
2025-11-27 02:03:13 -0300

    Atualização Conta Banco

--- src-projects/AulaExtra_SistemaBanco/ContaBanco.java
+++ src-projects/AulaExtra_SistemaBanco/ContaBanco.java
@@ -12,11 +12,35 @@ public class ContaBanco {
     private double saldo;
     private boolean Status;
 
-    public void abrirConta(){
-    Status = true;
+    public ContaBanco(){
+    
     }
 
-    public void fecharConta(){
+
+    public void abrirConta(){
+    Status = true;
+        int Valor = 0;
+        System.out.println("Bem Vindo ao Nosso Banco");
+        System.out.println("Digite seu nome: ");
+        Scanner teclado = new Scanner(System.in);
+        Dono = teclado.nextLine();
+        System.out.println("Digite o Tipo de conta:");
+        System.out.println("Digite [1] para Conta Corrente ou Digite [2] para Conta Poupança: ");
+        Valor = teclado.nextInt();
+        if(Valor == 1){
+            Tipo = "CC";
+        }
+        else if(Valor == 2){
+            Tipo = "CP";
+        }
+        else{
+            System.out.println("Resposta incorreta! Selecione [1] ou [2]!");
+        }
+
+
+    }
+
+    public void setFecharConta(){
         if(saldo == 0){
         Status = false;
             System.out.println("Sua Conta foi Fechada");
@@ -28,25 +52,35 @@ public class ContaBanco {
             System.out.println("Saque todo o valor da sua conta para fechar");
         }
     }
-    public double depositar(){
-        double deposito = 0;
+    public double setDepositar(){
         System.out.println("Digite o valor que deseja depositar: ");
         Scanner teclado = new Scanner(System.in);
-        deposito = teclado.nextDouble();
-        return deposito;
+        saldo += teclado.nextDouble();
+        return saldo;
     }
-public double sacar(){
-    double saque = 0;
+public double SetSacar(){
         if(saldo > 0) {
             System.out.println("Digite o valor que deseja sacar: ");
             Scanner teclado = new Scanner(System.in);
-            saque = teclado.nextDouble();
+            saldo -= teclado.nextDouble();
         }
         else {
             System.out.println("Sem saldo Disponivel para sacar ");
         }
-    return saque;
+    return saldo;
     }
 
+    public double SetPagarMensal(){
+        if(Tipo.equals("CC")){
+            saldo = saldo - 12;
+        }
+        else{
+            saldo = saldo - 20;
+        }
+        return saldo;
+    }
 
+    public int getNumConta() {
+        return NumConta;
+    }
 }
--- src-projects/AulaExtra_SistemaBanco/Main.java
+++ src-projects/AulaExtra_SistemaBanco/Main.java
@@ -3,6 +3,9 @@ package AulaExtra_SistemaBanco;
 public class Main {
     public static void main(String[] args) {
 
+        ContaBanco c1 = new ContaBanco();
+c1.abrirConta();
+        c1.setDepositar();
 
 
     }
